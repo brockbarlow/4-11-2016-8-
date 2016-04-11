1414ef4d530ed10d24dc2acfd4c1f168e9fb6f56
bbarlowAIEYr1 <brock.barlow@students.aie.edu.au>
2016-04-11 15:27:43 -0500

    respawn works, game shows amount of items collected

--- Super Ball 628/Assets/Scripts/movement.cs
+++ Super Ball 628/Assets/Scripts/movement.cs
@@ -4,16 +4,27 @@ using UnityEngine.UI;
 
 public class movement : MonoBehaviour {
 
+    public GameObject[] Collects;
     private float movementSpeed = 10f; 
     private float jumpHeight = 7f; 
     bool canJump = true; 
     private Rigidbody rb;
+    Vector3 respawnPoint;
+    private int maxItems; 
+    private int currentItems;
+    public Text currentText;
+    public Text doorText;
   
 	void Start () {
+        currentItems = 0;
+        setCurrentText();
+        doorText.text = "";
+        maxItems = Collects.Length;
         rb = GetComponent<Rigidbody>();
+        respawnPoint = transform.position;
 	}
 
-	void FixedUpdate () {
+	void Update () {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
         Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
@@ -40,128 +51,53 @@ public class movement : MonoBehaviour {
         {
             canJump = true;
         }
-    }
 
-    /*void OnCollisionEnter(Collision other)  //Used to detect Enemies who collide with the player
-    {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Respawner"))
         {
-            Die();
+            resetCollect();
+            resetPlayer();
         }
-
-        else if(other.gameObject.CompareTag("Ground"))
-        {
-            jump = true;
-        }
-    }*/
-}
-
-/*
-    public GameObject[] Coll;   //All the collectables in the level
-    public GameObject deathParticals;
-    public Text winText;
-
-    int maxScore;
-    int currentScore;          //Number of collectables
-    Vector3 spawn;              //Spawn points
-    Vector2 movement;           //Used for moving the player
-    float maxSpeed = 10;
-    int currentLevel;
-
-    void Awake()
-    {
-        currentLevel = LevelController.currentLevel;
-        winText.text = "";
-        maxScore = Coll.Length;
-        currentScore = maxScore;
-        spawn = transform.position;                     //Spawn point
-       
-        movement = new Vector2(h, v);
-
-        if(playerRigidbody.position.y <= -25f)
-        {
-            Die();
-        }
-
-        if (currentLevel >= 4)
-        {
-            winText.text = "You Win!!!";
-        }
-    }
-
-    void Die()//Death and set to spawn point
-    {
-        Instantiate(deathParticals, transform.position, Quaternion.identity);
-        transform.position = spawn;
-        ResetCollect();
-        currentScore = maxScore;
-    }
-
-    void OnTriggerEnter(Collider other) //Used to see of what the player has triggered an object
-    {
-        if (other.gameObject.CompareTag("Collect"))//Collecting object
-        {
-            other.gameObject.SetActive(false);
-            currentScore -= 1;
-
-        }
-
-        if (other.transform.tag == ("Goal") && currentScore <= 0)//Goal trigger
-        {
-            LevelController.CompleteLevel();
-        } 
-    }
-
-    void OnCollisionEnter(Collision other)  //Used to detect Enemies who collide with the player
-    {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            Die();
-        }
-
-        else if(other.gameObject.CompareTag("Ground"))
-        {
-            jump = true;
-        }
-    }
-
-    void ResetCollect()//Resetting the collectables to active
-    {
-        for(int i = 0; i < Coll.Length ; i++)
-        {
-            Coll[i].SetActive(true);
-        }
-    }
-}*/
-
-/*
-    public Text countText;
-    public Text winText;
-    private int count;
-
-    void Start()
-    {
-        count = 0;
-        SetCountText();
-        winText.text = "";
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Item"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
+            currentItems++;
+            setCurrentText();
+        }
+
+        if (other.gameObject.CompareTag("Door"))
+        {
+            if (currentItems != maxItems)
+            {
+                doorText.text = "Your not ready...";
+            }
         }
     }
 
-    void SetCountText()
+    void setCurrentText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 30)
+        currentText.text = "Count: " + currentItems.ToString();
+
+        if (currentItems == maxItems)
         {
-            winText.text = "You Win!";
+            doorText.text = "The door opens...";
         }
     }
-}*/
+
+    void resetCollect()
+    {
+        for (int i = 0; i < Collects.Length; i++)
+        {
+            Collects[i].SetActive(true);
+        }
+    }
+
+    void resetPlayer()
+    {
+        currentItems = 0;
+        transform.position = respawnPoint;
+    }
+}
